d39f18bdac5a51f81d25cedfaa5d314dccff3948
afonsofrancof <afonso@francof.net>
2024-10-17 18:39:51 +0100

    Fixed grid of photos

--- lib/model/checksum.g.dart
+++ lib/model/checksum.g.dart
@@ -39,7 +39,7 @@ const ChecksumSchema = CollectionSchema(
   getId: _checksumGetId,
   getLinks: _checksumGetLinks,
   attach: _checksumAttach,
-  version: '3.1.0+1',
+  version: '3.1.8',
 );
 
 int _checksumEstimateSize(
